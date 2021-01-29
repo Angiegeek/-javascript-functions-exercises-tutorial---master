8c4dfe6c18fd8fcf2e6546d166dc74c4aea59b93
Angiegeek <avm03021993@gmail.com>
2021-01-29 21:59:54 +0000

    solucion ejercicio 5

--- exercises/05-Defining-vs-Calling-a-function/app.js
+++ exercises/05-Defining-vs-Calling-a-function/app.js
@@ -1,5 +1,9 @@
 //Define the function called "multi" that expects 2 parameters:
-
+function multi (number1,number2) {
+    return number1 * number2;
+}
+let number1= 7
+let number2= 53812212 
 
   //don't edit anything below this line
   var returnValue = multi(7,53812212);
